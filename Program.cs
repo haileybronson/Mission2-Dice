b9650b64fb86a453d6c7814d0fabce96fe3a178e
haileybronson <haileybronson@HAILEYBRONS41E8>
2024-01-17 10:41:51 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿//hailey bronson, section 3
+//for when you need to reference other classes 
+using C_Fun;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        System.Console.WriteLine("Welcome to the dice throwing stimulator!");
+
+        //Allow the user to choose how many times the “dice” will be thrown
+        System.Console.WriteLine("How many dice rolls would you like to simulate ?");
+
+        /*Then, once you have the
+        number of rolls, pass that number to a second class that has a method that simulates the roll of
+        the dice for the number of times that the user specified.*/
+        int numRolls = int.Parse(System.Console.ReadLine());
+        /*create as normal new variable and then pass into the class you will reference
+        as you instantiate the object*/
+
+        // Instantiating the object with the number of rolls
+        playGame pg = new playGame(numRolls);
+        printHistogram(pg.rollSums, numRolls);
+
+        System.Console.WriteLine("Thank you for using the dice throwing simulator. Goodbye!");
+    }
+
+    /*In the first class, use that array to print a histogram (using
+    the * character) that shows the total percentage each number was rolled. Each * will represent
+    1% of the total rolls. */
+    private static void printHistogram(int[] rollSums, int totalRolls)
+    {
+        System.Console.WriteLine("Dice Roll Histogram:");
+        for (int i = 0; i < rollSums.Length; i++)
+        {
+            System.Console.Write($"{i + 2}: ");
+            int percentage = (rollSums[i] * 100) / totalRolls;
+            for (int j = 0; j < percentage; j++)
+            {
+                System.Console.Write("*");
+            }
+            System.Console.WriteLine();
+        }
+    }
+
+}
---
+++ playGame.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Fun
+{
+    internal class playGame
+    {
+        //concerns with public??
+        public int numRolls;
+        public int[] rollSums;
+
+        //built-in method 
+        private Random rand;
+
+        //constructor
+        public playGame(int numRolls) 
+        {
+            this.numRolls = numRolls;
+            this.rollSums = new int[11];
+            /* This array will store the amount of times each sum is rolled, 2-12,
+             essentially there are 11 potential numbers to be rolled*/
+            rand = new Random();
+
+            rollDice();
+
+        }
+        //take out static because we want it used on program.cs
+        private void rollDice()
+        {
+            for (int i = 0; i < numRolls; i++) //pass the given input number here - 
+            {
+                //can I just make one "die" that is 1-13? or do I have to include both dice 
+                int firstDie = rand.Next(1, 7); // First dice roll
+                int secondDie = rand.Next(1, 7); // Second dice roll
+                int sum = firstDie + secondDie; // Sum of both rolls
+
+                rollSums[sum - 2]++;
+                /*If I roll a 6, then rollSums[4] will hold the "6" count, because 
+                you are holding numbers between 2-12 within the array, and the array starts 
+                at [0]
+                [0] is 2, [1] is 3, [2] is 4, etc. */
+            }
+        }
+    }
+}
